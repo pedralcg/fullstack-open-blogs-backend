444583345ec71c817870ab10f4a0de53f47793f7
Pedro Alcoba <pedralcg@gmail.com>
2025-06-03 21:15:23 +0200

    Part04a - Ejer 4.4 Auxiliary Functions and Unit Test, paso 2

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,14 +1,97 @@
-const { test, describe } = require('node:test') // Importa test y describe de node:test
-const assert = require('node:assert') // Importa la librería assert
-const listHelper = require('../utils/list_helper') // Importa el módulo listHelper
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+const listHelper = require('../utils/list_helper')
 
-// Define un bloque de descripción para agrupar pruebas relacionadas con listHelper
+// Bloque de pruebas para la función dummy
 describe('list helper', () => {
-  // Caso de prueba para la función dummy
   test('dummy returns one', () => {
-    const blogs = [] // Define un array vacío de blogs como parámetro de prueba
+    const blogs = []
+    const result = listHelper.dummy(blogs)
+    assert.strictEqual(result, 1)
+  })
+})
 
-    const result = listHelper.dummy(blogs) // Llama a la función dummy
-    assert.strictEqual(result, 1) // Verifica que el resultado sea estrictamente igual a 1
+// Bloque de pruebas para totalLikes
+describe('total likes', () => {
+  // Caso de prueba 1: Lista con un solo blog
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  // Caso de prueba 2: Lista vacía
+  const emptyList = []
+
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes(emptyList)
+    assert.strictEqual(result, 0)
+  })
+
+  // Caso de prueba 3: Lista con varios blogs
+  const blogs = [
+    {
+      _id: "5a422a851b54a676234d17f7",
+      title: "React patterns",
+      author: "Michael Chan",
+      url: "https://reactpatterns.com/",
+      likes: 7,
+      __v: 0
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: "5a422b3a1b54a676234d17f9",
+      title: "Canonical string representation of JSON",
+      author: "Edsger W. Dijkstra",
+      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+      likes: 12,
+      __v: 0
+    },
+    {
+      _id: "5a422b891b54a676234d17fa",
+      title: "First class tests",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.html",
+      likes: 10,
+      __v: 0
+    },
+    {
+      _id: "5a422ba71b54a676234d17fb",
+      title: "TDD harms architecture",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+      likes: 0,
+      __v: 0
+    },
+    {
+      _id: "5a422bc61b54a676234d17fc",
+      title: "Type wars",
+      author: "Robert C. Martin",
+      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+      likes: 2,
+      __v: 0
+    }
+  ]
+
+  test('of a bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 36) // 7+5+12+10+0+2 = 36
   })
 })
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  // Si la lista de blogs está vacía, la suma de likes es 0.
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  // Usa el método reduce para sumar los likes de cada blog.
+  const sum = blogs.reduce((total, blog) => total + blog.likes, 0)
+  return sum
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
